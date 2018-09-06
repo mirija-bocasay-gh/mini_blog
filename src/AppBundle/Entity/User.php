be051ef0917b523ddea2199345dc2018e46b74e4
Mirija <mandriantsilaizina@bocasay.com>
2018-09-06 12:07:23 +0300

    Authentication system : bases

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,22 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * Class User
+ * @package AppBundle\Entity
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
